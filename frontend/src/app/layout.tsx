cd7977bbe3ecb1b18262e0c7b573174a000e4a6c
THANIGAI-VELAN <thanigaivelan404@gmail.com>
2026-08-22 13:19:04 +0530

    fix(layout): suppress hydration warning on html tag to support browser extension attribute injection

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" className="light" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
